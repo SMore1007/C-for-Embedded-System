d7489cf44832ee78dcaee8c4fb6cc820bbff21f4
Sagar More <sagarmore1007@gmail.com>
2025-02-22 19:58:30 +0530

    Number Guessing Game

---
+++ 02_Simple_Calculator.c
@@ -0,0 +1,16 @@
+/*
+Project Name: Simple Calculator
+Author Name:  Sagar More
+Description:  A simple calculator which can do the addition,
+              substraction, Multiplication Division
+*/
+
+#include<stdio.h>
+
+
+int main()
+{
+    
+
+    return 0;
+}
