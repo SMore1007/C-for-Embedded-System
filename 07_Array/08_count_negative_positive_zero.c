52ddf198a4c06de4135e3b50b2b743685f67f047
Sagar More <sagarmore1007@gmail.com>
2025-03-08 16:20:58 +0530

    Count the Neg, Pos, Zero in the array

---
+++ 07_Array/08_count_negative_positive_zero.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main()
+{
+    int pos = 0;
+    int neg = 0;
+    int zero = 0;
+    int arr[10] = {1,-2,3,4,0,5,23,-45,0, -9};
+
+    int len = sizeof(arr)/ sizeof(arr[0]);
+    printf("Len: %d\n",len);
+
+    for (int i = 0; i < len; i++)
+    {
+        if (arr[i] > 0)
+        {
+            pos++;
+        }
+        else if (arr[i] < 0)
+        {
+            neg++;
+        }
+        else if (arr[i] == 0)
+        {
+            zero++;
+        }
+    }
+    printf("Positive Numbers: %d\n", pos);
+    printf("Negative numbers: %d\n", neg);
+    printf("Zeros: %d\n", zero);
+    return 0;
+}
