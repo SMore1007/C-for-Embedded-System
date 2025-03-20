f0d65e31842b90364d6bc37c162cb99249b76084
Sagar More <sagarmore1007@gmail.com>
2025-03-20 15:29:51 +0530

    Structure

--- 09_Structures/08_SortTheItemDataInCodeUsingStruct.c
+++ 09_Structures/08_SortTheItemDataInCodeUsingStruct.c
@@ -17,20 +17,28 @@ int main()
     {
         printf("Item %d\n", i + 1);
         printf("code, Name, Quantity\n");
-        scanf(" %d%s%d", &it[i].code, &it[i].subTopicDefinition, it[i].qty);
+        scanf(" %d%s%d", &it[i].code, &it[i].subTopicDefinition, &it[i].qty);
     }
     
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n - i - 1; j++)
+        for (int j = i + 1; j < n; j++)
         {
-            if (it[j] < it[j + 1])
+            if (it[i].code > it[j].code)
             {
-                /* code */
+                t = it[i];
+                it[i] = it[j];
+                it[j] = t;
             }
-            
         }
-        
     }
     
+    printf("After sorting on code\n");
+
+    printf("Code\tName\tQuantity\n");
+    for (int i = 0; i <n; i++)
+    {
+        printf("%d\t%s\t%d\n", it[i].code, it[i].subTopicDefinition, it[i].qty);
+    }
+    return 0;
 }
